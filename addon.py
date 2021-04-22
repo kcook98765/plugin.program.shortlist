5c8eef73ed0d65bf8b04aae2444a34f75c046e03
kcook98765 <56806582+kcook98765@users.noreply.github.com>
2021-04-21 17:24:45 -0700

    matrix changes

--- addon.py
+++ addon.py
@@ -13,7 +13,7 @@ from database import *
 base_url = sys.argv[0]
 
 __addon__       = xbmcaddon.Addon(id='plugin.program.shortlist')
-__addondir__    = translatePath( __addon__.getAddonInfo('profile') )
+__addondir__    = xbmcvfs.translatePath( __addon__.getAddonInfo('profile') )
 __language__     = __addon__.getLocalizedString
 
 addon_handle = int(sys.argv[1])
